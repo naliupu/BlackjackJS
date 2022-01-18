d0724d894b708a2ac4afba5b8f74b7922c462836
Cristian Cossio <cossiosoto5@gmail.com>
2022-01-18 17:20:24 -0500

    Juego terminado

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -4,9 +4,10 @@
 
 /** Funciones anonimas auto-invocadas */
 
-/** funcion flech */
-(() => {
-  'use strict'; //Habilita las opciones estrictas de JS.
+/** funcion flecha */
+const myModule = (() => {
+  //Habilita las opciones estrictas de JS.
+  "use strict";
 
   /**
    * Baraja:
@@ -18,27 +19,39 @@
   let deck = [];
 
   /** Tipos de cartas. */
-  const types = ["C", "D", "H", "S"];
-
-  /** Cartas especiales. */
-  const specials = ["A", "J", "Q", "k"];
+  const types = ["C", "D", "H", "S"],
+    /** Cartas especiales. */
+    specials = ["A", "J", "Q", "k"];
 
   /** Puntos */
-  let pointsPlayer = 0,
-    pointsComputer = 0;
+  let pointsPlayers = [];
 
   /** Manejo del DOM HTML */
-  const btnPedir = document.querySelector("#btnAskCart");
-  const btnStop = document.querySelector("#btnStop");
-  const btNew = document.querySelector("#btnNew");
+  const btnPedir = document.querySelector("#btnAskCart"),
+    btnStop = document.querySelector("#btnStop"),
+    btNew = document.querySelector("#btnNew");
 
-  const divCartPlayer = document.querySelector("#jugador-cartas");
-  const divCartComputer = document.querySelector("#computadora-cartas");
+  const divCartsPlayers = document.querySelectorAll(".divCarts"),
+    scoreView = document.querySelectorAll("#scoreView");
 
-  const scoreView = document.querySelectorAll("#scoreView");
+  /** Inicializa el juego*/
+  const initializeGame = (numPlayers = 2) => {
+    deck = createDeck();
+    pointsPlayers = [];
+    for (let i = 0; i < numPlayers; i++) {
+      pointsPlayers.push(0);
+    }
+
+    scoreView.forEach((elem) => (elem.innerHTML = 0));
+    divCartsPlayers.forEach((elem) => (elem.innerHTML = ""));
+
+    btnPedir.disabled = false;
+    btnStop.disabled = false;
+  };
 
   /** Crea una baraja de cartas. */
   const createDeck = () => {
+    deck = [];
     for (let i = 2; i <= 10; i++) {
       for (let type of types) {
         deck.push(i + type);
@@ -50,22 +63,15 @@
         deck.push(special + type);
       }
     }
-    //   console.log(deck);// Baraja de cartas ordenado
-    deck = _.shuffle(deck); //Utilize la libreria underscore , para que la baraja sea aleaotoria
-    // console.log(deck);
+    return _.shuffle(deck);
   };
-  createDeck();
 
   /** Pedir una carta de la baraja. */
   const askCart = () => {
     if (deck.length === 0) {
       throw "No hay cartas en la baraja";
     }
-    // .pop: coge el ultimo elemento del array y lo elimina
-    const cart = deck.pop();
-    // console.log(cart);
-    // console.log(deck);
-    return cart;
+    return deck.pop();
   };
 
   /** Obtener el valor de la carta. EJM 2S = 2 */
@@ -76,38 +82,25 @@
 
     //Operación ternaria
     return isNaN(value) ? (value === "A" ? 11 : 10) : value * 1;
-
-    // let puntos = 0;
-
-    // // isNaN: Evalua si lo que hay en los parentesis es un numero o no
-    // if (isNaN(value)) {
-    //   console.log("No es un numero!");
-    //   puntos = value === "A" ? 11 : 10;
-    // } else {
-    //   console.log("Es un numero");
-    //   // Multiplicamos el valor * 1 , para psarlo a tipode dato numerico, para poderlo sumar
-    //   puntos = value * 1;
-    // }
-    // console.log(puntos);
   };
 
-  /** Computadora */
-  const turnComputer = (pointsMinimum) => {
-    do {
-      const cart = askCart();
-      pointsComputer = pointsComputer + valueCart(cart);
-      scoreView[1].innerHTML = pointsComputer;
+  /** Acumular puntos de los jugadores */
+  const accumulatePoint = (cart, turn) => {
+    pointsPlayers[turn] = pointsPlayers[turn] + valueCart(cart);
+    scoreView[turn].innerHTML = pointsPlayers[turn];
+    return pointsPlayers[turn];
+  };
 
-      const uploadCart = document.createElement("img");
-      uploadCart.classList.add("cartas");
-      uploadCart.src = `./assets/cartas/${cart}.png`;
-      divCartComputer.append(uploadCart);
+  const createCart = (cart, turn) => {
+    console.log(cart, turn);
+    const uploadCart = document.createElement("img");
+    uploadCart.classList.add("cartas");
+    uploadCart.src = `./assets/cartas/${cart}.png`;
+    divCartsPlayers[turn].append(uploadCart);
+  };
 
-      if (pointsMinimum > 21) {
-        break;
-      }
-    } while (pointsComputer < pointsMinimum && pointsComputer <= 21);
-
+  const determinarWinner = () => {
+    const [pointsMinimum, pointsComputer] = pointsPlayers;
     setTimeout(() => {
       if (pointsComputer === pointsMinimum) {
         alert("EMPATE");
@@ -118,65 +111,46 @@
       } else {
         alert("PERDISTE");
       }
-    }, 10);
+    }, 200);
   };
 
-  // Eventos
-  // Nota
-  // cuando agregamos una function como argumento de otra function, se le conoce como callback, puede ser una funcion tradicional o una funcion flecha
+  /** Computadora */
+  const turnComputer = (pointsMinimum) => {
+    let pointsComputer = 0;
+    do {
+      const cart = askCart();
+      pointsComputer = accumulatePoint(cart, pointsPlayers.length - 1);
+      createCart(cart, pointsPlayers.length - 1);
+    } while (pointsComputer < pointsMinimum && pointsComputer <= 21);
+
+    determinarWinner();
+  };
 
   btnPedir.addEventListener("click", () => {
     const cart = askCart();
-    pointsPlayer = pointsPlayer + valueCart(cart);
-    scoreView[0].innerHTML = pointsPlayer;
+    const pointsPlayers = accumulatePoint(cart, 0);
+    createCart(cart, 0);
 
-    const uploadCart = document.createElement("img");
-    uploadCart.classList.add("cartas");
-    uploadCart.src = `./assets/cartas/${cart}.png`;
-    divCartPlayer.append(uploadCart);
-
-    if (pointsPlayer > 21) {
+    if (pointsPlayers > 21) {
       console.warn("Jugador perdio");
       btnPedir.disabled = true;
       btnStop.disabled = true;
-      turnComputer(pointsPlayer);
-    } else if (pointsPlayer === 21) {
+      turnComputer(pointsPlayers);
+    } else if (pointsPlayers === 21) {
       console.warn("21, genial");
       btnPedir.disabled = true;
       btnStop.disabled = true;
-      turnComputer(pointsPlayer);
+      turnComputer(pointsPlayers);
     }
   });
 
   btnStop.addEventListener("click", () => {
     btnPedir.disabled = true;
     btnStop.disabled = true;
-    turnComputer(pointsPlayer);
-    // if(pointsPlayer){}
+    turnComputer(pointsPlayers[0]);
   });
 
-  btNew.addEventListener("click", () => {
-    console.clear();
-    deck = [];
-    deck = createDeck();
-    pointsPlayer = 0;
-    pointsComputer = 0;
-    scoreView[0].innerText = 0;
-    scoreView[1].innerText = 0;
-    divCartPlayer.innerHTML = "";
-    divCartComputer.innerHTML = "";
-    btnPedir.disabled = false;
-    btnStop.disabled = false;
-    console.log(deck);
-
-    // deck = [];
-    // console.log(deck);
-    // deck = createDeck();
-    // console.log(deck);
-  });
+  return {
+    newGame: initializeGame, //Retorna para poder llamarlo desde un agente externo
+  };
 })();
-
-
-/** funcion normal */
-// (function(){
-// })
